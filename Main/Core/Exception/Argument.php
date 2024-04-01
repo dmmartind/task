a2d07638c77ae097b83adb8af48cff605051d010
David Martin <dmmartind@gmail.com>
2024-04-01 07:22:21 -0500

    Create Argument.php
    core Argument exception class

---
+++ Main/Core/Exception/Argument.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Main\Core\Exception {
+
+    use Main\Core as Core;
+
+    /**
+     * Class Argument
+     * @package Framework\Core\Exception
+     */
+    class Argument extends Core\Exception
+    {
+
+    }
+}
