d7683ba3e84ef7a686a5e217fd4711dabe5e8568
David Martin <dmmartind@gmail.com>
2024-04-01 07:17:20 -0500

    Create Service.php
    Service class exception

---
+++ Main/Database/Exception/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Main\Database\Exception {
+
+    use Main\Database as Database;
+
+    /**
+     * Class Service
+     * @package Framework\Database\Exception
+     */
+    class Service extends Database\Exception
+    {
+
+    }
+}
+
+
