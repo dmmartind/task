3753e1e3a417842c41ca44e43147940cadba0a20
David Martin <dmmartind@gmail.com>
2024-04-01 07:18:35 -0500

    Create Exception.php
    main Exception class for database

---
+++ Main/Database/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Main\Database {
+
+    use Main\Core as Core;
+
+    /**
+     * Class Exception
+     * @package Framework\Database
+     */
+    class Exception extends Core\Exception
+    {
+
+    }
+}
