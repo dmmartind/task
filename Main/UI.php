b00fe8ad08129ff0964fde3e5e8c291322d07f1d
David Martin <dmmartind@gmail.com>
2024-04-01 07:10:32 -0500

    Create UI.php
    created the abstract UI class to write basic html via php code

---
+++ Main/UI.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Main {
+    abstract class UI
+    {
+        private $documentName;
+        private $jsArr = [];
+        private $cssArr = [];
+
+
+
+        public function start_html()
+        {
+            echo("<html>");
+        }
+
+        public function end_html()
+        {
+            echo $this->includeJS();
+            echo("</html>");
+        }
+
+        public function startBody()
+        {
+            echo "<body>";
+        }
+
+        public function endBody()
+        {
+            echo "</body>";
+        }
+
+        public function defaultHeader()
+        {
+            $out = <<<EOF
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{$this->documentName}</title>
+EOF;
+
+            echo($out);
+            $this->includeCSS();
+            echo "</header>";
+        }
+
+        abstract public function Display();
+        abstract public function includeJS();
+        abstract public function includeCSS();
+
+    }
+}
+
+
+
+?>
