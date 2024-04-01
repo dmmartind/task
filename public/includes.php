43d47edae0f663f43c96bacf021671f78a2741ec
David Martin <dmmartind@gmail.com>
2024-04-01 07:08:43 -0500

    Create includes.php
    includes add the instances for the app

---
+++ public/includes.php
@@ -0,0 +1,19 @@
+<?php
+define("DEBUG", true);
+define("APP_PATH", dirname(__DIR__));
+
+require("../Main/core.php");
+
+
+Main\Core::initialize();
+
+$configuration = new Main\Configuration([
+                                                 "type" => "ini",
+                                                 "class" => "database"
+                                             ]);
+
+Main\Registry::set("Configuration", $configuration->initialize());
+
+$database = new Main\Database([]);
+Main\Registry::set("Database", $database->initialize());
+
