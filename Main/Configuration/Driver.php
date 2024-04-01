4589ea6a5d6a3d7f55549c8486c79038380410c1
David Martin <dmmartind@gmail.com>
2024-04-01 07:40:27 -0500

    Create Driver.php
    Driver class that is for more abstract functions are house

---
+++ Main/Configuration/Driver.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Main\Configuration
+{
+    class Driver
+    {
+        protected $_parsed = [];
+        public function initialize()
+        {
+            //var_dump("driver init");
+            return $this;
+        }
+
+    }
+
+}
+
+
