8ca5d83b5e59aa925815668333c040f24a55aa7d
David Martin <dmmartind@gmail.com>
2024-04-07 21:54:57 -0500

    Update task.js
    added eventListener load

---
+++ public/assets/js/task.js
@@ -0,0 +1,13 @@
+(function () {
+
+
+    /******************************************************************************************
+     *
+     *   windowLoadHandler ()
+     *  arg:
+     *   desc: handle function to reload the task list and set event listener to the toggle all button and the
+     *   new task input
+     */
+    window.addEventListener('load', windowLoadHandler, false);
+    
+}());
