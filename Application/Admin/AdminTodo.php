466756bd8267e97f8d18b4b3c51a4b701cc66da7
David Martin <dmmartind@gmail.com>
2024-04-15 17:00:06 -0500

    Update AdminTodo.php
    **added session start
    added references for session, header,and registry
    **added getUserList to get a list of users to view except the admin users
    **added getTaskCount to get the tasks count for a user
    **added getAllUsers to directly get all users from the users table
    ** added getUserById gets the user row using id

--- Application/Admin/AdminTodo.php
+++ Application/Admin/AdminTodo.php
@@ -3,11 +3,90 @@
 
 namespace Application\Admin
 {
+    if(session_id() === "") session_start();
+    use Main\Session as Session;
+    use Main\Header as Header;
+    use Main\Registry as Registry;
+
+
     class AdminTodo
     {
-        public function getUserList()
+        public static function getUserList()
         {
-            
+            $id = Session::getUserID();
+            $user = self::getUserById($id);
+            $header = new Header();
+
+
+            if (!Session::isUserLoggedIn())
+                header('/login');
+
+            if ($user['isAdmin'] != 1)
+                header('/login');
+
+            $adminID = $user['id'];
+            $usersWithTodos = self::getAllUsers();
+            $userList = [];
+
+            foreach ($usersWithTodos as $user) {
+                if ($user['isAdmin'] == 1)
+                    continue;
+                $userList[$user['id']]['name'] = $user['name'];
+                $taskCount = 0;
+                $taskCount = self::getTaskCount($user['id']);
+
+                $userList[$user['id']]['taskCount'] = $taskCount;
+            }
+            return $userList;
+
+        }
+
+        public static function getTaskCount($id)
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->count();
+                return $query;
+            }
+
+
+        }
+
+        public static function getAllUsers()
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->all();
+                return $query;
+            }
+
+        }
+
+        public static function getUserById(int $id)
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->where("id = ?", "{$id}")
+                    ->first();
+                return $query;
+            }
+
+
         }
 
     }
