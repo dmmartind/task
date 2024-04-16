efeec369c7f977e319adff8c2804182307d7e1a2
David Martin <dmmartind@gmail.com>
2024-04-15 22:48:46 -0500

    Update dashboard.php
    added temp code while developing the admin portion of the app

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,4 +1,19 @@
 <?php
 
 require("includes.php");
-include_once("../Application/Admin/AdminTaskList.php");
+include_once("../Application/Frontend/AdminTaskList.php");
+
+if(isset($_GET['id']))
+{
+    $id = Main\ArrayMethods::array_get($_POST, 'id', -1);
+
+    $task = new Application\Frontend\AdminTask($id);
+    $task->Display();
+
+}
+else
+{
+
+    $task = new Application\Frontend\AdminTaskList();
+    $task->Display();
+}
