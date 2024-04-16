b35d86b04c7dbdc4c1f343d7fa443b9fc32e0a01
David Martin <dmmartind@gmail.com>
2024-04-15 23:14:27 -0500

    Create AdminTodo.php
    **moved AdminTodo to the frontend folder
    **added getAuth function to get the current logged in user row from the db
    **added getUserById get user row by id
    ** added getAllUsers to directly query the db for user list
    **added getTaskCount return count of task for a user
    **added GetUserList to get the full list

---
+++ Application/Frontend/AdminTodo.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace Application\Frontend
+{
+    if(session_id() === "") session_start();
+    use Main\Session as Session;
+    use Main\Header as Header;
+    use Main\Registry as Registry;
+
+
+    class AdminTodo
+    {
+        public static function getUserList()
+        {
+            $id = Session::getUserID();
+            $user = self::getUserById($id);
+            $header = new Header();
+
+
+            if (!Session::isUserLoggedIn())
+            {
+                error_log("hurt1");
+                header('/login');
+            }
+
+
+            if ($user['isAdmin'] != 1)
+            {
+                error_log("hurt2");
+                header('/login');
+            }
+
+
+            $adminID = $user['id'];
+            $usersWithTodos = self::getAllUsers();
+            $userList = [];
+
+            foreach ($usersWithTodos as $user) {
+                if ($user['isAdmin'] == 1)
+                    continue;
+                $userList[$user['id']]['name'] = $user['name'];
+                $taskCount = 0;
+                $taskCount = self::getTaskCount($user['id']);
+
+                $userList[$user['id']]['taskCount'] = $taskCount;
+            }
+            return $userList;
+
+        }
+
+        public static function getTaskCount($id)
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->count();
+                return $query;
+            }
+
+
+        }
+
+        public static function getAllUsers()
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->all();
+                return $query;
+            }
+
+        }
+
+        public static function getUserById(int $id)
+        {
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                $query = $database->query()
+                    ->from("users")
+                    ->where("id = ?", "{$id}")
+                    ->first();
+                return $query;
+            }
+
+
+        }
+
+        public static function getAuth()
+        {
+            $id = Session::getUserID();
+            return Todo::getUserById($id);
+        }
+
+    }
+}
+
+
