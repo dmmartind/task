a0364fb2745aaa8a623ee0e7eb3ba2226062781b
David Martin <dmmartind@gmail.com>
2024-04-17 09:05:23 -0500

    Update dashboard.php
    **added reference to the Profile class to handles the UI for the profile page
    **added attributes to check to see if the profile page is being requested
    **create the profile class wi the user id as the argument
    **call to the display to display the profile pagge.
    **also changed the second check to check the Get instaed iof the POST
    **same for the arraymethods call I changed the POST to a GET

--- public/dashboard.php
+++ public/dashboard.php
@@ -2,10 +2,24 @@
 
 require("includes.php");
 include_once("../Application/Frontend/AdminTaskList.php");
+include_once("../Application/Frontend/Profile.php");
 
-if(isset($_GET['id']))
+if(isset($_GET['cmd']) && isset($_GET['id']))
 {
-    $id = Main\ArrayMethods::array_get($_POST, 'id', -1);
+    $cmd = Main\ArrayMethods::array_get($_GET, 'cmd', -1);
+    $id = Main\ArrayMethods::array_get($_GET, 'id', -1);
+    if($cmd == 'profile')
+    {
+        $profile = new Application\Frontend\Profile($id);
+        $profile->Display();
+    }
+
+
+
+}
+else if(isset($_GET['id']))
+{
+    $id = Main\ArrayMethods::array_get($_GET, 'id', -1);
 
     $task = new Application\Frontend\AdminTask($id);
     $task->Display();
