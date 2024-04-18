3369b0d4ecf478ade4717ac3034d2ba66d8b56fb
David Martin <dmmartind@gmail.com>
2024-04-18 18:10:40 -0500

    Update Profile.php
    fixed the Profile UI

--- Application/Frontend/Profile.php
+++ Application/Frontend/Profile.php
@@ -55,14 +55,15 @@ EOF;
             $auth['name'] = "Admin";
 
             $html = <<<EOF
+<!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
-    <link rel="stylesheet" href="assets/css/listtask.css">
-    <link rel="stylesheet" href="assets/css/Articles-Cards-images.css">
+    <link rel="stylesheet" href="assets\css\profile.css">
+    <link rel="stylesheet" href="assets/css/list.css?v={CURRENT_TIMESTAMP}">
     
 </head>
 
@@ -83,55 +84,54 @@ EOF;
 
     </div>
 </nav>
-    <div class="container py-4 py-xl-5">
-        <div class="row gy-4">
-            <div class="col-xl-6 col-xxl-6">
-                <div class="card">
-                    <div class="card-body p-4">
-                        <h4 class="mb-4 mt-0">Contact details</h4>
-                                    <div class="col-md-6">
+    <div class="container">
+        <div class="row">            
+                <div class="card1">
+                    <div class="card1-body">
+                        <h4>Contact details</h4>
+                                    <div class="field1">
                                         <label>Name</label>
                                         <input type="text" class="form-control" placeholder="" aria-label="First name">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="field2">
                                         <label>Email</label>
                                         <input type="email" class="form-control" placeholder="">
                                     </div>
-                                    <div class="d-lg-flex justify-content-lg-end">
+                                    <div class="field3">
                                         <button class="btn btn-light me-2" type="button">Submit</button>
                                     </div>
                                 
                     </div>
                 </div>
-            </div>
-            <div class="col-xl-6" >
-                <div class="card">
-                    <div class="card-body p-4">
-                        <h4 class="my-4">Change Password</h4>
+            
+            
+                <div class="card2">
+                    <div class="card2-body">
+                        <h4>Change Password</h4>
 								<!-- Old password -->
-								<div class="col-md-6">
+								<div class="field1">
 									<label for="exampleInputPassword1" class="form-label">Old password *</label>
 									<input type="password" class="form-control" id="exampleInputPassword1">
 								</div>
 								<!-- New password -->
-								<div class="col-md-6">
+								<div class="field2">
 									<label for="exampleInputPassword2" class="form-label">New password *</label>
 									<input type="password" class="form-control" id="exampleInputPassword2">
 								</div>
 								<!-- Confirm password -->
-								<div class="col-md-6">
+								<div class="field3">
 									<label for="exampleInputPassword3" class="form-label">Confirm Password *</label>
 									<input type="password" class="form-control" id="exampleInputPassword3">
 								</div>
-                                <div class="d-lg-flex justify-content-lg-end">
+                                <div class="field4">
                                     <button class="btn btn-light me-2" type="button">Submit</button>
                                 </div>
                     </div>
                 </div>
-            </div>
+            
         </div>
     </div>
-    <script src="assets/js/bootstrap.min.js"></script>
+    
 </body>
 
 </html>
