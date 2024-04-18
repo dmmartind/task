79c1078b3b508d71bc017d6f41e7f046001cf72b
David Martin <dmmartind@gmail.com>
2024-04-18 04:13:51 -0500

    Create TodoMail.php
    TodoMail class handles the actual contents of an email
    **construct takes the attributes of the contents of the email
    **getMessage builds the message of the email
    **getHeaders might be deleted; was used for the php mail function
    **SendEmail function to call sendMail function

---
+++ Application/Frontend/TodoMail.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Application\Frontend
+{
+    class TodoMail
+    {
+        protected $to;
+        protected $subject;
+        protected $message;
+        protected $headers;
+        protected $from;
+        protected $cc;
+        const HEADERS1 =  "MIME-Version: 1.0" . "\r\n";
+        const HEADERS2 = "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+
+        public function __construct($to, $subject, $name, $title, $priority, $from, $cc = "")
+        {
+            $this->to = $to;
+            $this->subject = $subject;
+            $this->getMessage($name, $title, $priority);
+            $this->from = $from;
+            $this->cc = $cc;
+        }
+
+        public function getMessage($name, $title, $priority)
+        {
+            $html = <<<EOF
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskManager Report: New Task Added to your List</title>
+</head>
+<body>
+<p>Hi {$name}, a new task called {$title} with the priority set to {$priority} has been added to your task list.</p>
+</body>
+</html>
+EOF;
+            $this->messages = $html;
+        }
+
+        public function getHeaders($from, $cc)
+        {
+            $tempHeader = self::HEADERS1;
+            $tempHeader .= self::HEADERS2;
+            $tempHeader .= "From: <{$from}> . \r\n";
+            $tempHeader .= "Cc: {$cc} . \r\n";
+            $this->headers = $tempHeader;
+
+        }
+
+        public function sendEmail()
+        {
+            sendMail($this->to,$this->subject,$this->message,$this->from, $this->cc);
+        }
+    }
+}
+
+
