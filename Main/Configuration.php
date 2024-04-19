a3d0b70cbb169478c8cdf5189af10a07c3e0c53e
David Martin <dmmartind@gmail.com>
2024-04-19 15:03:29 -0500

    Update Configuration.php
    removed debugging

--- Main/Configuration.php
+++ Main/Configuration.php
@@ -12,20 +12,14 @@ namespace Main
 
         public function __construct(Array $conf)
         {
-            //var_dump("construct");
             $this->_type = ArrayMethods::array_get($conf, 'type');
             $this->_name = ArrayMethods::array_get($conf, 'class');
-            //var_dump($this->_type);
-            //var_dump($this->_name);
-            //var_dump("---------------------------------------");
         }
 
         public function initialize()
         {
-            //var_dump("config init");
             if(!$this->_type)
             {
-                //var_dump($this->_type);
                 throw new Exception\Argument("Invalid type");
             }
 
@@ -33,8 +27,6 @@ namespace Main
             {
                 case "ini":
                 {
-                    //var_dump("call driver/ini");
-                    //var_dump($this->_name);
                     return new Configuration\Driver\Ini($this->_name);
                     break;
                 }
