cbdff120688f6b5ca1a79e5e47f9ce2a8bf2d265
David Martin <dmmartind@gmail.com>
2024-04-19 15:03:36 -0500

    Update ArrayMethods.php
    removed debugging

--- Main/ArrayMethods.php
+++ Main/ArrayMethods.php
@@ -8,25 +8,17 @@ namespace Main
 
         public static function array_get(Array $arr, $key, $default = null)
         {
-            //var_dump("start");
             if(!is_array($arr))
                 return $default;
-            //var_dump("step1");
             if(is_null($key))
                 return $arr;
-            //var_dump("step2");
-            //var_dump(array_keys($arr));
-            //var_dump(in_array($key,array_keys($arr)));
+
             if(in_array($key,array_keys($arr)))
             {
-                //var_dump("good");
-                //var_dump($key);
                 return $arr[$key];
             }
             else
             {
-                //var_dump("not good");
-                //var_dump($key);
                 return $default;
             }
         }
