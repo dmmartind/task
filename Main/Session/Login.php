5d5c69eb0b4f9990d1f2b061e2a183e1c4474163
David Martin <dmmartind@gmail.com>
2024-04-19 15:01:42 -0500

    Update Login.php
    removed debugging

--- Main/Session/Login.php
+++ Main/Session/Login.php
@@ -19,17 +19,14 @@ namespace Main\Session {
 
         public function processLogin()
         {
-            error_log("start");
             // Define $username and $password
             $email = $this->email = ArrayMethods::array_get($_POST, 'email', "");
             $password = $this->password = ArrayMethods::array_get($_POST, 'password', "");
-            error_log("step");
             // Establishing Connection with Server by passing server_name, user_id and password as a parameter
             $database = Registry::get("Database");
             $database = $database->connect();
             if ($database->_isValidService()) {
 
-                error_log("step2");
                 $creds = $this->filterCreds($email, $password, $database);
 
                 $this->email = $creds['email'];
@@ -42,22 +39,17 @@ namespace Main\Session {
 
                 if (count($all) > 0) {
                     $hashed_password = $all[0]["password"];
-                    error_log("step3");
                     if (password_verify($password, $hashed_password)) {
                         // Set the session variables
                        $this->setSession($all[0]['id'], $email);
 
-                        error_log("step4");
-
                         // Redirect to the user's dashboard
-						error_log("loggggged");
                         header("Location: dashboard.php");
                         exit;
                     } else {
                         return [
                             'error' => "email or Password is invalid! Please re-enter..."
                         ];
-                        //header("Location: index.php");
                     }
                 } else {
                     return [
