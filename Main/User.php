f16d473660ad18b41c675b03c5b64fcb4354d29b
David Martin <dmmartind@gmail.com>
2024-04-19 15:01:23 -0500

    Update User.php
    removed debugging

--- Main/User.php
+++ Main/User.php
@@ -26,19 +26,14 @@ namespace Main
 
         public function updateUser($postArr)
         {
-            //error_log("updateUser");
             $id = Session::getUserID();
             $user = User::getUserById($id);
             if (Session::isUserLoggedIn() && $user) {
-                //error_log("loggedin");
-                //error_log(print_r($postArr));
                 if (!ArrayMethods::array_get($postArr, 'name', 0) && !ArrayMethods::array_get($postArr, 'email', 0)) {
                     return;
                 }
-                //error_log("extract");
                 $name = ArrayMethods::array_get($postArr, 'name', 0);
                 $email = ArrayMethods::array_get($postArr, 'email', 0);
-                //error_log("database");
                 $database = Registry::get("Database");
                 if (!$database->_isValidService()) {
                     $database = $database->connect();
@@ -47,38 +42,29 @@ namespace Main
                     ->from("users")
                     ->where("id = ?", "{$id}")
                     ->save(['name' => $name, 'email' => $email]);
-                //error_log("result");
-                //error_log($query);
             }
         }
 
         public function updatePassword($postArr)
         {
-            error_log("updatePassword");
             $id = Session::getUserID();
             $user = User::getUserById($id);
             if (Session::isUserLoggedIn() && $user) {
-               error_log("loggedin");
-                error_log(print_r($postArr));
+
                 if (!ArrayMethods::array_get($postArr, 'old_password', 0) && !ArrayMethods::array_get($postArr, 'new_password', 0) &&
                     !ArrayMethods::array_get($postArr, 'confirm_password', 0)) {
                     return;
                 }
-                error_log("extract");
+
                 $oldPass = ArrayMethods::array_get($postArr, 'old_password', 0);
                 $newPass = ArrayMethods::array_get($postArr, 'new_password', 0);
                 $confirmPass = ArrayMethods::array_get($postArr, 'confirm_password', 0);
-                error_log($newPass);
-                error_log($confirmPass);
-                error_log("before");
+
 
                 if($newPass == $confirmPass)
                 {
-                    error_log("confirmed!!!");
                     if(password_verify($oldPass, $user['password']))
                     {
-                        error_log("old password correct!!");
-                        error_log("passed");
                         $hashNewPass = password_hash($newPass, PASSWORD_DEFAULT);
                         $database = Registry::get("Database");
                         if (!$database->_isValidService()) {
@@ -88,8 +74,6 @@ namespace Main
                             ->from("users")
                             ->where("id = ?", "{$id}")
                             ->save(['password' => $hashNewPass]);
-                        //error_log("result");
-                        //error_log($query);
 
                     }
 
