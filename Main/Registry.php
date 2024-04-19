06d0daf7eecaa6f012c166c2231c599060d2afbe
David Martin <dmmartind@gmail.com>
2024-04-19 15:01:53 -0500

    Update Registry.php
    removed debugging

--- Main/Registry.php
+++ Main/Registry.php
@@ -17,22 +17,14 @@ namespace Main
 
         public static function set($key, $instance = null)
         {
-            //var_dump("set");
-            //var_dump($key);
-            //var_dump($instance);
             self::$_instances[$key] = $instance;
         }
 
         public static function get($key, $default = null)
         {
-            //var_dump("get");
-            //var_dump(self::$_instances);
-            //var_dump($key);
             if (isset(self::$_instances[$key])) {
-                //var_dump("test");
                 return self::$_instances[$key];
             }
-            //var_dump("fail");
             return $default;
         }
 
