0a0c2dd73957ac6198e445239c55f90a40c3ecc7
David Martin <dmmartind@gmail.com>
2024-04-19 07:30:48 -0500

    Update Profile.php
    **added the names to the fields for the profile and the passwords.
    **added a call getUserById
    **added calling getUserById to get the row so that the profile fields will be filled with the name and email

--- Application/Frontend/Profile.php
+++ Application/Frontend/Profile.php
@@ -1,10 +1,11 @@
 <?php
 
 
-namespace Application\Frontend
-{
+namespace Application\Frontend {
+
     use Application\UI as UI;
     use Main\ArrayMethods as ArrayMethods;
+
     class Profile extends UI
     {
         public function __construct()
@@ -19,7 +20,6 @@ namespace Application\Frontend
     
 EOF;
             echo $html;
-
         }
 
         public function printNav()
@@ -28,7 +28,6 @@ EOF;
 
 EOF;
             echo $html;
-
         }
 
         public function printSection()
@@ -37,7 +36,6 @@ EOF;
 
 EOF;
             echo $html;
-
         }
 
         public function printFooter()
@@ -46,13 +44,14 @@ EOF;
 
 EOF;
             echo $html;
-
         }
 
         public function Display()
         {
             $auth = [];
             $auth['name'] = "Admin";
+
+            $auth = AdminTodo::getUserById(ArrayMethods::array_get($_GET, 'id', ""));
 
             $html = <<<EOF
 <!DOCTYPE html>
@@ -63,75 +62,80 @@ EOF;
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
     <link rel="stylesheet" href="assets\css\profile.css">
-    <link rel="stylesheet" href="assets/css/list.css?v={CURRENT_TIMESTAMP}">
-    
+
+
 </head>
 
 <body>
-<nav class="header">
-    <div class="logo">TaskManager</div>
-    <div class="header-right">
-        <div class="username" style="font-size: 1rem;">{$auth['name']}</div>
-        <a class="" href="dashboard.php">List</a>
-        <a class="active" href="dashboard.php?cmd=profile&id=30">Profile</a>
-        <form id="logout-form" action="logout.php" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
-        <a class="#contact" href="{{ route('logout') }}"
-           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    <nav class="header">
+        <div class="logo">TaskManager</div>
+        <div class="header-right">
+            <div class="username" style="font-size: 1rem;">{$auth['name']}</div>
+            <a class="" href="dashboard.php">List</a>
+            <a class="active" href="dashboard.php?cmd=profile&id=30">Profile</a>
+            <form id="logout-form" action="logout.php" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+            <a class="#contact" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             Logout
         </a>
 
-    </div>
-</nav>
+        </div>
+    </nav>
     <div class="container">
-        <div class="row">            
-                <div class="card1">
-                    <div class="card1-body">
-                        <h4>Contact details</h4>
-                                    <div class="field1">
-                                        <label>Name</label>
-                                        <input type="text" class="form-control" placeholder="" aria-label="First name">
-                                    </div>
-                                    <div class="field2">
-                                        <label>Email</label>
-                                        <input type="email" class="form-control" placeholder="">
-                                    </div>
-                                    <div class="field3">
-                                        <button class="btn btn-light me-2" type="button">Submit</button>
-                                    </div>
-                                
-                    </div>
+        <div class="row">
+            <div class="card1">
+                <div class="card1-body">
+                    <h4>Contact details</h4>
+                    <form class="form-1" method="post" action="dashboard.php">
+                        <div class="field1">
+                            <label>Name</label>
+                            <input name=name type="text" class="form-control" value={$auth['name']} placeholder="" aria-label="First name">
+                        </div>
+                        <div class="field2">
+                            <label>Email</label>
+                            <input name=email type="email" value={$auth['email']} class="form-control" placeholder="">
+                            <input name=cmd type="hidden" value="update_user" >
+                        </div>
+                        <div class="field3">
+                            <button class="btn btn-light me-2" type="submit">Submit</button>
+                        </div>
+                    </form>
+
                 </div>
-            
-            
-                <div class="card2">
-                    <div class="card2-body">
-                        <h4>Change Password</h4>
-								<!-- Old password -->
-								<div class="field1">
-									<label for="exampleInputPassword1" class="form-label">Old password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword1">
-								</div>
-								<!-- New password -->
-								<div class="field2">
-									<label for="exampleInputPassword2" class="form-label">New password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword2">
-								</div>
-								<!-- Confirm password -->
-								<div class="field3">
-									<label for="exampleInputPassword3" class="form-label">Confirm Password *</label>
-									<input type="password" class="form-control" id="exampleInputPassword3">
-								</div>
-                                <div class="field4">
-                                    <button class="btn btn-light me-2" type="button">Submit</button>
-                                </div>
-                    </div>
+            </div>
+
+
+            <div class="card2">
+                <div class="card2-body">
+                    <h4>Change Password</h4>
+                    <form class="form-2" method="post" action="dashboard.php">
+                        <!-- Old password -->
+                        <div class="field1">
+                            <label for="exampleInputPassword1" class="form-label">Old password *</label>
+                            <input type="password" class="form-control" name="old_password">
+                        </div>
+                        <!-- New password -->
+                        <div class="field2">
+                            <label for="exampleInputPassword2" class="form-label">New password *</label>
+                            <input type="password" class="form-control" name="new_password">
+                        </div>
+                        <!-- Confirm password -->
+                        <div class="field3">
+                            <label for="exampleInputPassword3" class="form-label">Confirm Password *</label>
+                            <input type="password" class="form-control" name="confirm_password">
+                            <input name=cmd type="hidden" value="update_password" >
+                        </div>
+                        <div class="field4">
+                            <button class="btn btn-light me-2" type="submit">Submit</button>
+                        </div>
+                    </form>
                 </div>
-            
+            </div>
+
         </div>
     </div>
-    
+
 </body>
 
 </html>
@@ -141,9 +145,6 @@ EOF;
 EOF;
 
             echo $html;
-
-
-
         }
 
         public function includeJS()
@@ -161,7 +162,6 @@ EOF;
 
 EOF;
             echo $html;
-
         }
 
     }
