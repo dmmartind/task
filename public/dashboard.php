f5c739ebc59911b72a86aeb23a649d73968aea60
David Martin <dmmartind@gmail.com>
2024-04-19 07:15:08 -0500

    Update dashboard.php
    **added a actions array with the two commands updateUser and update_password.
    
    **added the logic to check the POST commands and create an instance of the User class and call the function

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,6 +4,29 @@ require("includes.php");
 include_once("../Application/Frontend/AdminTaskList.php");
 include_once("../Application/Frontend/Profile.php");
 
+
+
+$actions = [
+    'update_user' => [
+        'object' => 'Main\User',
+        'method' => 'updateUser'
+    ],
+    'update_password' => [
+        'object' => 'Main\User',
+        'method' => 'updatePassword'
+    ],
+];
+
+if ( Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_POST, 'cmd',""), false) )
+{
+    $use_array = Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_POST, 'cmd',""), false);
+    $class = Main\ArrayMethods::array_get($use_array,'object', NULL);
+    $obj = new $class();
+
+    $method = Main\ArrayMethods::array_get($use_array, 'method', NULL);
+    $obj->$method($_POST);
+}
+
 if(isset($_GET['cmd']) && isset($_GET['id']))
 {
     $cmd = Main\ArrayMethods::array_get($_GET, 'cmd', -1);
