415c50a18cf26c60448d080e4c63bc6d2a091027
David Martin <dmmartind@gmail.com>
2024-04-19 21:05:03 -0500

    Update Syntax.php
    added docblocks

--- Main/Configuration/Exception/Syntax.php
+++ Main/Configuration/Exception/Syntax.php
@@ -5,6 +5,10 @@ namespace Main\Configuration\Exception
 {
     use Main\Configuration as Configuration;
 
+    /**
+     * Class Syntax
+     * @package Main\Configuration\Exception
+     */
     class Syntax extends Configuration\Exception
     {
 
