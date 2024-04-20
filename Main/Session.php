43bffcdc188177d8ab706baff583892fcd4604d2
David Martin <dmmartind@gmail.com>
2024-04-19 21:03:33 -0500

    Update Session.php
    added docblocks

--- Main/Session.php
+++ Main/Session.php
@@ -6,26 +6,47 @@ namespace Main
     if(session_id() === "") session_start();
 
     use Main\ArrayMethods as ArrayMethods;
+
+    /**
+     * Class Session
+     * @package Main
+     */
     class Session
     {
+        /**
+         * @var
+         */
         private $token;
 
+        /**
+         * @return array|mixed|null
+         */
         public static function getCSRFToken()
         {
             return ArrayMethods::array_get($_SESSION, 'token', 0);
 
         }
 
+        /**
+         * @return array|mixed|null
+         */
         public static function isUserLoggedIn()
         {
             return ArrayMethods::array_get($_SESSION, 'loggedin', false);
         }
 
+        /**
+         * @return array|mixed|null
+         */
         public static function getUserID()
         {
             return ArrayMethods::array_get($_SESSION, 'id', -1);
         }
 
+        /**
+         * @param $id
+         * @param $email
+         */
         public static function setSession($id, $email)
         {
             $_SESSION['loggedin'] = true;
@@ -34,6 +55,9 @@ namespace Main
             self::generateCSRF();
         }
 
+        /**
+         *
+         */
         public static function generateCSRF()
         {
             if(!ArrayMethods::array_get($_SESSION, 'token', 0))
@@ -42,6 +66,11 @@ namespace Main
             }
         }
 
+        /**
+         * @param int $length
+         * @return string
+         * @throws \Exception
+         */
         public static function random($length = 16)
         {
             $string = '';
@@ -56,6 +85,9 @@ namespace Main
             return $string;
         }
 
+        /**
+         * @return bool
+         */
         public static function getAuth()
         {
             $id = Session::getUserID();
