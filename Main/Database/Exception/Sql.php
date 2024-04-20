21e47abddd9f0f5a58930148182993422c263e5b
David Martin <dmmartind@gmail.com>
2024-04-19 21:04:40 -0500

    Update Sql.php
    added docblocks

--- Main/Database/Exception/Sql.php
+++ Main/Database/Exception/Sql.php
@@ -5,6 +5,10 @@ namespace Main\Database\Exception
 {
     use Main\Database as Database;
 
+    /**
+     * Class Sql
+     * @package Main\Database\Exception
+     */
     class Sql extends Database\Exception
     {
 
