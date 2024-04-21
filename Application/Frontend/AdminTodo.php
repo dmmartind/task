ab76b1f86e51f2a00bc9c83466eeccf0aeb603c0
David Martin <dmmartind@gmail.com>
2024-04-21 08:51:45 -0500

    Update AdminTodo.php
    changed the SQL builder to select * from todos where userID = $id and call count to return the count of rows for the number of tasks
    **added getList function
    **added getTodosByID function

--- Application/Frontend/AdminTodo.php
+++ Application/Frontend/AdminTodo.php
@@ -67,8 +67,10 @@ namespace Application\Frontend
             }
             if ($database->_isValidService()) {
                 $query = $database->query()
-                    ->from("users")
+                    ->from("todos")
+                    ->where("userID= ?", $id)
                     ->count();
+
                 return $query;
             }
 
@@ -95,6 +97,87 @@ namespace Application\Frontend
             }
             return false;
 
+
+        }
+
+        /**
+         * getList
+         * Desc: returns a user's task list
+         * @return array
+         */
+        function getList()
+        {
+            error_log("getList");
+            error_log("test");
+            error_log(print_r($_REQUEST, true));
+            if (Session::isUserLoggedIn() === null)
+            {
+                header('/login');
+            }
+
+            $id = Session::getUserId();
+            $todos = $this->getTodosByID($id);
+            if ($todos === null)
+                return [];
+            $result = [];
+
+            foreach ($todos as $info) {
+                $id = $info['id'];
+                $title = $info['title'];
+                $completed = $info['completed'];
+                $guid = $info['guid'];
+                $priority = $info['priority'];
+                $dbId = $info['id'];
+                $userId = $info['userID'];
+                $completed = ($completed == 0) ? false : true;
+
+                $temArr = [
+                    'id' => $id,
+                    'title' => $title,
+                    'completed' => $completed,
+                    'guid' => $guid,
+                    'priority' => $priority,
+                    'dbId' => $dbId,
+                    'userId' => $userId
+                ];
+                $result[] = $temArr;
+            }
+            header("Content-Type: application/json");
+            echo json_encode($result);
+
+        }
+
+        /**
+         * getTodosByID
+         * DESC: Utility function top return a task query object by id
+         * @param int $id
+         *
+         */
+        public function getTodosByID(int $id)
+        {
+            error_log("getTodosByID");
+            error_log($id);
+
+            if (!is_int($id))
+                return null;
+            $database = Registry::get("Database");
+            if (!$database->_isValidService()) {
+                $database = $database->connect();
+            }
+            if ($database->_isValidService()) {
+                try {
+                    $query = $database->query()
+                        ->from("todos")
+                        ->where("userid = ?", "{$id}")
+                        ->order("priority", "desc")
+                        ->all();
+                    return $query;
+
+                    //return $query;
+                } catch (QueryException $e) {
+                    return null;
+                }
+            }
 
         }
 
