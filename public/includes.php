1c986fe1793a54f8644b7d8b5898b2a999222bc0
David Martin <dmmartind@gmail.com>
2024-04-22 19:01:37 -0500

    Update includes.php
    **added DB_config configuration
    **added mail configuration
    **added registry set for mail
    **added registry set for DB
    **added mail instance and Registry set

--- public/includes.php
+++ public/includes.php
@@ -8,15 +8,25 @@ require("../Main/ArrayMethods.php");
 
 Main\Core::initialize();
 
-$configuration = new Main\Configuration([
+$DB_config = new Main\Configuration([
                                                  "type" => "ini",
                                                  "class" => "database"
                                              ]);
 
-Main\Registry::set("Configuration", $configuration->initialize());
+$MAIL_config = new Main\Configuration([
+                                        "type" => "ini",
+                                        "class" => "mail"
+                                    ]);
+
+Main\Registry::set("DBConfiguration", $DB_config->initialize());
+
+Main\Registry::set("MAILConfiguration", $MAIL_config->initialize());
 
 $database = new Main\Database([]);
 Main\Registry::set("Database", $database->initialize());
+
+$mail = new Main\Mailer([]);
+Main\Registry::set("Mailer", $mail->initialize());
 
 $login = new Main\Session\Login();
 
@@ -25,6 +35,10 @@ Main\Registry::set("Login", $login);
 $register = new Main\Session\Register();
 
 Main\Registry::set("Register", $register);
+
+
+
+
 
 
 
