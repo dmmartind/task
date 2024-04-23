92b622b7ba6cbf2bbedc55555fc4766615dc6b56
David Martin <dmmartind@gmail.com>
2024-04-22 22:39:25 -0500

    Update Header.php
    remove docblocks

--- Main/Header.php
+++ Main/Header.php
@@ -5,73 +5,40 @@ namespace Main
 {
     use Main\ArrayMethods as ArrayMethods;
 
-    /**
-     * Class Header
-     * @package Main
-     */
+
     class Header
     {
 
-        /**
-         * @var array|mixed|null
-         */
+
         private $host;
-        /**
-         * @var array|mixed|null
-         */
+
         private $userAgent;
-        /**
-         * @var array|mixed|null
-         */
+
         private $accept;
-        /**
-         * @var array|mixed|null
-         */
+
         private $acceptLang;
-        /**
-         * @var array|mixed|null
-         */
+
         private $acceptEncc;
-        /**
-         * @var array|mixed|null
-         */
+
         private $contentType;
-        /**
-         * @var array|mixed|null
-         */
+
         private $requestWith;
-        /**
-         * @var array|mixed|null
-         */
+
         private $contentLength;
-        /**
-         * @var array|mixed|null
-         */
+
         private $origin;
-        /**
-         * @var array|mixed|null
-         */
+
         private $connection;
-        /**
-         * @var array|mixed|null
-         */
+
         private $referer;
-        /**
-         * @var array|mixed|null
-         */
+
         private $fetchDest;
-        /**
-         * @var array|mixed|null
-         */
+
         private $fetchMode;
-        /**
-         * @var array|mixed|null
-         */
+
         private $fetchSite;
 
-        /**
-         * Header constructor.
-         */
+
         public function __construct()
         {
             $headerArr = getallheaders();
@@ -93,121 +60,91 @@ namespace Main
 
 
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getHost()
         {
             return $this->host;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getUserAgent()
         {
             return $this->userAgent;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getAccept()
         {
             return $this->accept;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getAcceptLang()
         {
             return $this->acceptLang;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getAcceptEncc()
         {
             return $this->acceptEncc;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getContentType()
         {
             return $this->contentType;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getRequestWith()
         {
             return $this->requestWith;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getContentLength()
         {
             return $this->contentLength;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getOrigin()
         {
             return $this->origin;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getConnection()
         {
             return $this->connection;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getReferer()
         {
             return $this->referer;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getFetchDest()
         {
             return $this->fetchDest;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getFetchMode()
         {
             return $this->fetchMode;
         }
 
-        /**
-         * @return array|mixed|null
-         */
+
         public function getFetchSite()
         {
             return $this->fetchSite;
         }
 
-        /**
-         * @return bool
-         */
+
         public function isAjax()
         {
             return 'XMLHttpRequest' == $this->getRequestWith();
