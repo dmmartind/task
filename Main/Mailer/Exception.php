6b6ba0c1e54fdb0d406f7f07abbd822333d3cf23
David Martin <dmmartind@gmail.com>
2024-04-22 19:12:40 -0500

    Create Exception.php
    added mailer exception class

---
+++ Main/Mailer/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Main\Mailer {
+
+    use Main\Core as Core;
+
+    /**
+     * Class Exception
+     * @package Framework\Database
+     */
+    class Exception extends Core\Exception
+    {
+
+    }
+}
