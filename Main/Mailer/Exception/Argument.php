97ce48e42d328cb5037095bc75d8f00ee5ff2d06
David Martin <dmmartind@gmail.com>
2024-04-22 19:12:20 -0500

    Create Argument.php
    added exception/argument  class

---
+++ Main/Mailer/Exception/Argument.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Main\Mailer\Exception {
+
+    use Main\Core as Core;
+
+    /**
+     * Class argument
+     * @package Framework\Database\Exception
+     */
+    class argument extends Core\Exception\Argument
+    {
+
+    }
+}
