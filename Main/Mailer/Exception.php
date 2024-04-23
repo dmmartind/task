df0bea42b5ba088a7c89e0f3456a47e9933852f2
David Martin <dmmartind@gmail.com>
2024-04-22 22:39:12 -0500

    Update Exception.php
    remove docblocks

--- Main/Mailer/Exception.php
+++ Main/Mailer/Exception.php
@@ -4,10 +4,7 @@ namespace Main\Mailer {
 
     use Main\Core as Core;
 
-    /**
-     * Class Exception
-     * @package Framework\Database
-     */
+
     class Exception extends Core\Exception
     {
 
