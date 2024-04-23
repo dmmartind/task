f0b47cf883e90a060ad878e888f5e7283d376c55
David Martin <dmmartind@gmail.com>
2024-04-22 22:40:46 -0500

    Update Argument.php
    remove docblocks

--- Main/Configuration/Exception/Argument.php
+++ Main/Configuration/Exception/Argument.php
@@ -4,10 +4,7 @@ namespace Main\Configuration\Exception
 {
     use Main\Core as Core;
 
-    /**
-     * Class Argument
-     * @package Main\Configuration\Exception
-     */
+
     class Argument extends Core\Exception\Argument
     {
 
