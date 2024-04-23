2f5d03d5e99c9a8a5a0e79313822cf2e135f74aa
David Martin <dmmartind@gmail.com>
2024-04-22 22:41:05 -0500

    Update Configuration.php
    remove docblocks

--- Main/Configuration.php
+++ Main/Configuration.php
@@ -6,35 +6,21 @@ namespace Main
     use Main\ArrayMethods as ArrayMethods;
     use Main\Configuration\Exception as Exception;
 
-    /**
-     * Class Configuration
-     * @package Main
-     */
+
     class Configuration
     {
-        /**
-         * @var array|mixed|null
-         */
-        protected $_type;
-        /**
-         * @var array|mixed|null
-         */
+               protected $_type;
+
         protected $_name;
 
-        /**
-         * Configuration constructor.
-         * @param array $conf
-         */
+
         public function __construct(Array $conf)
         {
             $this->_type = ArrayMethods::array_get($conf, 'type');
             $this->_name = ArrayMethods::array_get($conf, 'class');
         }
 
-        /**
-         * @return Configuration\Driver\Ini
-         * @throws Exception\Argument
-         */
+
         public function initialize()
         {
             if(!$this->_type)
