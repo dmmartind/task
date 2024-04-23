c0cb77fac7673c1d710ec2e7a3029fb05b64c7a0
David Martin <dmmartind@gmail.com>
2024-04-22 22:41:01 -0500

    Update Driver.php
    remove docblocks

--- Main/Configuration/Driver.php
+++ Main/Configuration/Driver.php
@@ -4,20 +4,13 @@
 namespace Main\Configuration
 {
 
-    /**
-     * Class Driver
-     * @package Main\Configuration
-     */
+
     class Driver
     {
-        /**
-         * @var array
-         */
+
         protected $_parsed = [];
 
-        /**
-         * @return $this
-         */
+
         public function initialize()
         {
             return $this;
