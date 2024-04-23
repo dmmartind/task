e898ccd8bb98c1c7e5259c0672d65220581c4337
David Martin <dmmartind@gmail.com>
2024-04-22 22:40:22 -0500

    Update Connector.php
    remove docblocks

--- Main/Database/Connector.php
+++ Main/Database/Connector.php
@@ -4,23 +4,16 @@ namespace Main\Database {
 
     use Main\Database\Exception as Exception;
 
-    /**
-     * Class Connector
-     * @package Framework\Database
-     */
+
     class Connector
     {
-        /**
-         * Connector constructor.
-         */
+
         public function __construct()
         {
             var_dump("construct for Database\connector");
         }
 
-        /**
-         * @return $this
-         */
+
         public function initialize()
         {
             return $this;
