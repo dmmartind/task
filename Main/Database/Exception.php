993b6431ecad873c90755ee4474c11c5a6620f26
David Martin <dmmartind@gmail.com>
2024-04-22 22:40:13 -0500

    Update Exception.php
    remove docblocks

--- Main/Database/Exception.php
+++ Main/Database/Exception.php
@@ -4,10 +4,7 @@ namespace Main\Database {
 
     use Main\Core as Core;
 
-    /**
-     * Class Exception
-     * @package Framework\Database
-     */
+
     class Exception extends Core\Exception
     {
 
