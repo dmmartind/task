beefcba5030cc1a7f0fc517caee50dfe149960b8
David Martin <dmmartind@gmail.com>
2024-04-25 20:34:45 -0500

    Update dashboard.php
    reformatting

--- public/dashboard.php
+++ public/dashboard.php
@@ -3,9 +3,6 @@
 require("includes.php");
 
 
-
-
-
 $actions = [
     'update_user' => [
         'object' => 'Main\User',
@@ -21,44 +18,40 @@ $actions = [
     ],
 ];
 
-if ( Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_POST, 'cmd',""), false) )
-{
-    $use_array = Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_POST, 'cmd',""), false);
-    $class = Main\ArrayMethods::array_get($use_array,'object', NULL);
+if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_POST, 'cmd', ""), false)) {
+    $use_array = Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_POST, 'cmd', ""), false);
+    $class = Main\ArrayMethods::array_get($use_array, 'object', null);
     $obj = new $class();
 
-    $method = Main\ArrayMethods::array_get($use_array, 'method', NULL);
+    $method = Main\ArrayMethods::array_get($use_array, 'method', null);
     $obj->$method($_POST);
 }
 
-if ( Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_GET, 'cmd',""), false) )
-{
-    $use_array = Main\ArrayMethods::array_get($actions,Main\ArrayMethods::array_get($_GET, 'cmd',""), false);
-    $class = Main\ArrayMethods::array_get($use_array,'object', NULL);
+if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_GET, 'cmd', ""), false)) {
+    $use_array = Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_GET, 'cmd', ""), false);
+    $class = Main\ArrayMethods::array_get($use_array, 'object', null);
     $obj = new $class();
 
-    $method = Main\ArrayMethods::array_get($use_array, 'method', NULL);
+    $method = Main\ArrayMethods::array_get($use_array, 'method', null);
     $obj->$method();
-}
-else if(Main\ArrayMethods::array_get($_GET, 'id',-1) !== -1)
-{
-    $id = Main\ArrayMethods::array_get($_GET, 'id', -1);
+} else {
+    if (Main\ArrayMethods::array_get($_GET, 'id', -1) !== -1) {
+        $id = Main\ArrayMethods::array_get($_GET, 'id', -1);
 
-    $task = new Application\Frontend\AdminTask($id);
-    $task->Display();
-
-}
-else {
-    $auth = Main\Session::getAuth();
-    if ($auth) {
-        if (Main\ArrayMethods::array_get($auth, 'isAdmin', 0) == 1) {
-            $task = new Application\Frontend\AdminTaskList();
-            $task->Display();
-        } else {
-            $task = new Application\Frontend\Task();
-            $task->Display();
-        }
+        $task = new Application\Frontend\AdminTask($id);
+        $task->Display();
     } else {
-        header("refresh:0; url=logout.php");
+        $auth = Main\Session::getAuth();
+        if ($auth) {
+            if (Main\ArrayMethods::array_get($auth, 'isAdmin', 0) == 1) {
+                $task = new Application\Frontend\AdminTaskList();
+                $task->Display();
+            } else {
+                $task = new Application\Frontend\Task();
+                $task->Display();
+            }
+        } else {
+            header("refresh:0; url=logout.php");
+        }
     }
 }
