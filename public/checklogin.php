aff0034dab924a6b1c08773d7f5e57e9def2e60f
David Martin <dmmartind@gmail.com>
2024-04-25 20:35:01 -0500

    Update checklogin.php
    reformatting

--- public/checklogin.php
+++ public/checklogin.php
@@ -1,16 +1,16 @@
 <?php
+
 require("includes.php");
 
-if(session_id() === "") session_start();
-$error=''; // Variable To Store Error Message
+if (session_id() === "") {
+    session_start();
+}
+$error = ''; // Variable To Store Error Message
 $success = '';
 
 if (isset($_POST['submit'])) {
-
     $login = Main\Registry::get('Login');
-    $message = ($result = $login->processLogin())?$result:[];
+    $message = ($result = $login->processLogin()) ? $result : [];
     $error = Main\ArrayMethods::array_get($message, 'error', "");
-
-
 }
 
