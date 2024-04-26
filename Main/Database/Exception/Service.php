f178a9eed311cb85270c5d10869a69d3dcf56fff
David Martin <dmmartind@gmail.com>
2024-04-25 20:37:00 -0500

    Update Service.php
    added docblocks

--- Main/Database/Exception/Service.php
+++ Main/Database/Exception/Service.php
@@ -6,6 +6,10 @@ namespace Main\Database\Exception {
     use Main\Database as Database;
 
 
+    /**
+     * Class Service
+     * @package Main\Database\Exception
+     */
     class Service extends Database\Exception
     {
 
