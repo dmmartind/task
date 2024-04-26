48262c468288a767f9c18628f92b0f9092520738
David Martin <dmmartind@gmail.com>
2024-04-25 20:34:29 -0500

    Update includes.php
    reformatting

--- public/includes.php
+++ public/includes.php
@@ -1,24 +1,29 @@
 <?php
+
 define("DEBUG", true);
 define("APP_PATH", dirname(__DIR__));
-$public = realpath (filter_input ( INPUT_SERVER , ' DOCUMENT_ROOT '));
+$public = realpath(filter_input(INPUT_SERVER, ' DOCUMENT_ROOT '));
 define("APP_PUBLIC", $public);
 
-require(APP_PATH . DIRECTORY_SEPARATOR ."Main" . DIRECTORY_SEPARATOR . "core.php");
-require(APP_PATH . DIRECTORY_SEPARATOR . "Main" . DIRECTORY_SEPARATOR .  "ArrayMethods.php");
+require(APP_PATH . DIRECTORY_SEPARATOR . "Main" . DIRECTORY_SEPARATOR . "core.php");
+require(APP_PATH . DIRECTORY_SEPARATOR . "Main" . DIRECTORY_SEPARATOR . "ArrayMethods.php");
 
 
 Main\Core::initialize();
 
-$DB_config = new Main\Configuration([
-                                                 "type" => "ini",
-                                                 "class" => "database"
-                                             ]);
+$DB_config = new Main\Configuration(
+    [
+        "type" => "ini",
+        "class" => "database"
+    ]
+);
 
-$MAIL_config = new Main\Configuration([
-                                        "type" => "ini",
-                                        "class" => "mail"
-                                    ]);
+$MAIL_config = new Main\Configuration(
+    [
+        "type" => "ini",
+        "class" => "mail"
+    ]
+);
 
 Main\Registry::set("DBConfiguration", $DB_config->initialize());
 
