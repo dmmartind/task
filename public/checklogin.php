67879d4b1d351d84e079b5da624e6f98c7b0113d
David Martin <dmmartind@gmail.com>
2024-04-26 07:27:40 -0500

    Update checklogin.php
    added comments

--- public/checklogin.php
+++ public/checklogin.php
@@ -1,13 +1,14 @@
 <?php
 
 require("includes.php");
-
+//session start
 if (session_id() === "") {
     session_start();
 }
 $error = ''; // Variable To Store Error Message
 $success = '';
 
+//if correct request, gets the login instance, calls the login processor and returns error if login fails
 if (isset($_POST['submit'])) {
     $login = Main\Registry::get('Login');
     $message = ($result = $login->processLogin()) ? $result : [];
