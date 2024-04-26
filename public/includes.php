caee14aab68c95518cc6ccbab029565cc7b6cdd5
David Martin <dmmartind@gmail.com>
2024-04-26 07:27:16 -0500

    Update includes.php
    added comments

--- public/includes.php
+++ public/includes.php
@@ -1,5 +1,5 @@
 <?php
-
+//set paths
 define("DEBUG", true);
 define("APP_PATH", dirname(__DIR__));
 $public = realpath(filter_input(INPUT_SERVER, ' DOCUMENT_ROOT '));
@@ -8,16 +8,17 @@ define("APP_PUBLIC", $public);
 require(APP_PATH . DIRECTORY_SEPARATOR . "Main" . DIRECTORY_SEPARATOR . "core.php");
 require(APP_PATH . DIRECTORY_SEPARATOR . "Main" . DIRECTORY_SEPARATOR . "ArrayMethods.php");
 
-
+//initialize core
 Main\Core::initialize();
 
+//set db configuration
 $DB_config = new Main\Configuration(
     [
         "type" => "ini",
         "class" => "database"
     ]
 );
-
+//set mail configuration
 $MAIL_config = new Main\Configuration(
     [
         "type" => "ini",
@@ -25,22 +26,24 @@ $MAIL_config = new Main\Configuration(
     ]
 );
 
+//set the instances of the db configuration to the registry
 Main\Registry::set("DBConfiguration", $DB_config->initialize());
-
+//set the instances of the main configuration to the registry
 Main\Registry::set("MAILConfiguration", $MAIL_config->initialize());
 
+//create the database instance and set it to the registry
 $database = new Main\Database([]);
 Main\Registry::set("Database", $database->initialize());
-
+//create the mailer instance and set it to the registry
 $mail = new Main\Mailer([]);
 Main\Registry::set("Mailer", $mail->initialize());
 
+//create the login instance and set it to the registry
 $login = new Main\Session\Login();
-
 Main\Registry::set("Login", $login);
 
+//create the register instance and set it to the registry
 $register = new Main\Session\Register();
-
 Main\Registry::set("Register", $register);
 
 
