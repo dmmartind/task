f1276a8229aa8a6e5b015279730f4533a70342be
David Martin <dmmartind@gmail.com>
2024-04-25 20:36:34 -0500

    Update Argument.php
    added docblocks

--- Main/Mailer/Exception/Argument.php
+++ Main/Mailer/Exception/Argument.php
@@ -5,6 +5,10 @@ namespace Main\Mailer\Exception {
     use Main\Core as Core;
 
 
+    /**
+     * Class Argument
+     * @package Main\Mailer\Exception
+     */
     class Argument extends Core\Exception\Argument
     {
 
