b89518c819d37a948ead0016ca769786294b016a
David Martin <dmmartind@gmail.com>
2024-04-25 20:37:16 -0500

    Update Connector.php
    added docblocks

--- Main/Database/Connector.php
+++ Main/Database/Connector.php
@@ -5,14 +5,24 @@ namespace Main\Database {
     use Main\Database\Exception as Exception;
 
 
+    /**
+     * Class Connector
+     * @package Main\Database
+     */
     class Connector
     {
 
+        /**
+         * Connector constructor.
+         */
         public function __construct()
         {
         }
 
 
+        /**
+         * @return $this
+         */
         public function initialize()
         {
             return $this;
