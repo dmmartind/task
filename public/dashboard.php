44231fa991d9ba24d2ae83745db7b5235387c59a
David Martin <dmmartind@gmail.com>
2024-04-26 07:27:24 -0500

    Update dashboard.php
    added comments

--- public/dashboard.php
+++ public/dashboard.php
@@ -2,7 +2,7 @@
 
 require("includes.php");
 
-
+//actions array to hold the cmd key, class namespace, and class method to call
 $actions = [
     'update_user' => [
         'object' => 'Main\User',
@@ -18,6 +18,7 @@ $actions = [
     ],
 ];
 
+//if cmd is found in the POST request, create the new class instance, and call the command
 if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_POST, 'cmd', ""), false)) {
     $use_array = Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_POST, 'cmd', ""), false);
     $class = Main\ArrayMethods::array_get($use_array, 'object', null);
@@ -26,7 +27,7 @@ if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_POST, 
     $method = Main\ArrayMethods::array_get($use_array, 'method', null);
     $obj->$method($_POST);
 }
-
+//if cmd is found in the GET request, create the new class instance, and call the command
 if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_GET, 'cmd', ""), false)) {
     $use_array = Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_GET, 'cmd', ""), false);
     $class = Main\ArrayMethods::array_get($use_array, 'object', null);
@@ -34,24 +35,22 @@ if (Main\ArrayMethods::array_get($actions, Main\ArrayMethods::array_get($_GET, '
 
     $method = Main\ArrayMethods::array_get($use_array, 'method', null);
     $obj->$method();
-} else {
-    if (Main\ArrayMethods::array_get($_GET, 'id', -1) !== -1) {
-        $id = Main\ArrayMethods::array_get($_GET, 'id', -1);
-
+} elseif(Main\ArrayMethods::array_get($_GET, 'id', -1) !== -1) {//if id query in the GET request
+        $id = Main\ArrayMethods::array_get($_GET, 'id', -1);//create AdminTask instance and call display
+//for the display of a users tsk list in the admin view
         $task = new Application\Frontend\AdminTask($id);
         $task->Display();
     } else {
-        $auth = Main\Session::getAuth();
+        $auth = Main\Session::getAuth();//else give me the admin page with the user list
         if ($auth) {
             if (Main\ArrayMethods::array_get($auth, 'isAdmin', 0) == 1) {
                 $task = new Application\Frontend\AdminTaskList();
                 $task->Display();
-            } else {
+            } else {//if not admin just give user their tassk list because they are a regular user
                 $task = new Application\Frontend\Task();
                 $task->Display();
             }
-        } else {
+        } else {//else failed and logout
             header("refresh:0; url=logout.php");
         }
     }
-}
