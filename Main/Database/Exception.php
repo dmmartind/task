77164bb4cb17ab47f8cdb583acde83da6294a883
David Martin <dmmartind@gmail.com>
2024-04-25 20:37:08 -0500

    Update Exception.php
    added docblocks

--- Main/Database/Exception.php
+++ Main/Database/Exception.php
@@ -5,6 +5,10 @@ namespace Main\Database {
     use Main\Core as Core;
 
 
+    /**
+     * Class Exception
+     * @package Main\Database
+     */
     class Exception extends Core\Exception
     {
 
