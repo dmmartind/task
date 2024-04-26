8ed4b425e1a12d7ebc7671abda668d9c340e6d7d
David Martin <dmmartind@gmail.com>
2024-04-25 20:37:04 -0500

    Update Argument.php
    added docblocks

--- Main/Database/Exception/Argument.php
+++ Main/Database/Exception/Argument.php
@@ -5,6 +5,10 @@ namespace Main\Database\Exception {
     use Main\Core as Core;
 
 
+    /**
+     * Class Argument
+     * @package Main\Database\Exception
+     */
     class Argument extends Core\Exception\Argument
     {
 
