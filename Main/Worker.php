cc540295f1b44753ea44f99c4211500d37c4e620
David Martin <dmmartind@gmail.com>
2024-04-28 09:33:32 -0500

    Update Worker.php
    ** changed the constructor to default the argument to empty string if nothing was given.
    **added getQueue to return the queue name
    **added setter for the queue name
    **added the process function
    that loops through calling the queue instance process command until the $item is 0

--- Main/Worker.php
+++ Main/Worker.php
@@ -4,23 +4,49 @@
 namespace Main
 {
     use Main\Worker\Exception\Argument as Argument;
+    use Main\Registry as Registry;
 
     class Worker
     {
         protected $queueName;
 
-        public function __construct($queue)
+        public function __construct($queue = "")
         {
             $this->queueName = $queue;
         }
 
+        /**
+         * @return string
+         */
+        public function getQueueName(): string
+        {
+            return $this->queueName;
+        }
+
+        /**
+         * @param string $queueName
+         */
+        public function setQueueName(string $queueName)
+        {
+            $this->queueName = $queueName;
+        }
+
+
+
         public function process()
         {
+            var_dump($this->queueName);
             $Queue = Registry::get($this->queueName);
-
+            $item = 1;
 
             try {
-                $item = $Queue->process();
+                while($item)
+                {
+                    error_log("Loop");
+                    $item = $Queue->process();
+                    error_log(print_r($item, true));
+                }
+
             }
             catch(Argument $e)
             {
@@ -28,8 +54,8 @@ namespace Main
             }
         }
     }
-    if($argc>1)
-        parse_str(implode('&',array_slice($argv, 1)), $_GET);
+
+
 
 }
 
