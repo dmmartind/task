5db4dddb4e2a10eed45eae28e1b0fe75f6becc63
David Martin <dmmartind@gmail.com>
2024-04-27 21:24:32 -0500

    Create Argument.php
    added Queue Argument exception

---
+++ Main/Queue/Exception/Argument.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ©2024 David Martin. All Rights Reserve.
+ */
+namespace Main\Queue\Exception {
+
+    use Main\Core as Core;
+
+
+    /**
+     * Class Argument
+     * @package Main\Mailer\Exception
+     */
+    class Argument extends Core\Exception\Argument
+    {
+
+    }
+}
