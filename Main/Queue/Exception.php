cbad9b458921ced3b2944e43a0a939ec428faaae
David Martin <dmmartind@gmail.com>
2024-04-27 21:24:46 -0500

    Create Exception.php
    added Queue exception

---
+++ Main/Queue/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ©2024 David Martin. All Rights Reserve.
+ */
+namespace Main\Queue {
+
+    use Main\Core as Core;
+
+
+    /**
+     * Class Exception
+     * @package Main\Mailer
+     */
+    class Exception extends Core\Exception
+    {
+
+    }
+}
