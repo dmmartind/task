9dff1544e508dca699b7675cd3280d2e0d02842a
David Martin <dmmartind@gmail.com>
2024-04-29 06:07:13 -0500

    Update EmailQueue.php
    **added docblocks
    **added function call markFailed to the catch block

--- Main/Queue/EmailQueue.php
+++ Main/Queue/EmailQueue.php
@@ -10,15 +10,28 @@ namespace Main\Queue {
     use Application\Frontend\TodoMail as TodoMail;
 
 
+    /**
+     * Class EmailQueue
+     * @package Main\Queue
+     */
     class EmailQueue extends Queue
     {
 
+        /**
+         * EmailQueue constructor.
+         * sets the clss prop with the name of the queue, which matches the queue of the current class
+         */
         public function __construct()
         {
             $this->queueDBTable = 'email_queue';
         }
 
 
+        /**
+         * adds attributes into the queue table to use in a process call once a job is in progress
+         * @param array $queueData
+         * @return mixed|void
+         */
         public function addItem(array $queueData)
         {
             if (!is_array($queueData)) {
@@ -44,11 +57,23 @@ namespace Main\Queue {
             }
         }
 
+        /**
+         * function to mark current queue as failed with the error message
+         * @param $item
+         * @param $errorMessageText
+         */
         public function markFailed($item, $errorMessageText)
         {
             $this->changeItemStatus($item['id'], 'failed', $errorMessageText);
         }
 
+        /**
+         * general function that handles the db functions that update the current queue item with the current status
+         * @param int $id
+         * @param string $status
+         * @param string $error
+         * @return mixed
+         */
         public function changeItemStatus(int $id, string $status, string $error = "")
         {
             $update['status'] = $status;
@@ -71,6 +96,10 @@ namespace Main\Queue {
             }
         }
 
+        /**
+         * called by the worker->process function do the work specific to the processed queue
+         * @return int
+         */
         public function process()
         {
             $item = $this->getItem();
@@ -85,10 +114,14 @@ namespace Main\Queue {
                 return 1;
             } catch (Exception $e) {
                 error_log($e->getMessage());
+                $this->markFailed($item, $e->getMessage());
             }
         }
 
 
+        /**
+         * @return mixed
+         */
         public function getItem()
         {
             $database = Registry::get("Database");
@@ -109,11 +142,17 @@ namespace Main\Queue {
             }
         }
 
+        /**
+         * @param $item
+         */
         public function markProcessed($item)
         {
             $this->changeItemStatus($item['id'], 'processing');
         }
 
+        /**
+         * @param $item
+         */
         public function markDone($item)
         {
             $this->changeItemStatus($item['id'], 'done');
