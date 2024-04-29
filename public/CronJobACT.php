5c8201934119992e3f07cc96adfb24d416fe10b1
David Martin <dmmartind@gmail.com>
2024-04-29 05:25:54 -0500

    Update CronJobACT.php
    removed debug

--- public/CronJobACT.php
+++ public/CronJobACT.php
@@ -4,9 +4,7 @@ require("includes.php");
 
 if($argc>1)
 {
-    error_log("worker!!!!");
     $worker = Main\Registry::get("Worker");
     $worker->setQueueName($argv[1]);
     $worker->process();
-    error_log(print_r($argv, true));
 }
