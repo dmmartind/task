886740a959c766d9e88b8c1115cbd48294d9dbe2
David Martin <dmmartind@gmail.com>
2024-04-29 05:53:37 -0500

    Update Queue.php
    **added docblocks

--- Main/Queue.php
+++ Main/Queue.php
@@ -1,18 +1,35 @@
 <?php
 
 
-namespace Main
-{
+namespace Main {
+
+    /**
+     * Class Queue
+     * @package Main
+     */
     abstract class Queue
     {
+        /**
+         * @var
+         */
         protected $queueDBTable;
 
+        /**
+         * Queue constructor.
+         */
         public function __construct()
         {
         }
 
+        /**
+         * @param array $queueData
+         * @return mixed
+         */
         abstract public function addItem(Array $queueData);
 
+        /**
+         * @return mixed
+         */
         abstract public function getItem();
 
     }
